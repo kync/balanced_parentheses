e2b60eac6508685e37f1e7967f2fe75c73879c91
ali <cs.alikoyuncu@gma>
2015-07-16 19:09:53 -0400

    The script solves the balance of parentheses problems

---
+++ balanced_parentheses.py
@@ -0,0 +1,39 @@
+﻿BRACKET_ROUND_OPEN = '('
+BRACKET_ROUND__CLOSE = ')'
+BRACKET_CURLY_OPEN = '{'
+BRACKET_CURLY_CLOSE = '}'
+BRACKET_SQUARE_OPEN = '['
+BRACKET_SQUARE_CLOSE = ']'
+
+TUPLE_OPEN_CLOSE = [(BRACKET_ROUND_OPEN,BRACKET_ROUND__CLOSE),
+                    (BRACKET_CURLY_OPEN,BRACKET_CURLY_CLOSE),
+                    (BRACKET_SQUARE_OPEN,BRACKET_SQUARE_CLOSE)]
+BRACKET_LIST = [BRACKET_ROUND_OPEN,BRACKET_ROUND__CLOSE,BRACKET_CURLY_OPEN,BRACKET_CURLY_CLOSE,BRACKET_SQUARE_OPEN,BRACKET_SQUARE_CLOSE]
+
+def balanced_parentheses(expression):
+    stack = list()
+    left = expression[0]
+    for ex in expression:
+        if ex not in BRACKET_LIST:
+            continue
+        skip = False
+        for bracket_couple in TUPLE_OPEN_CLOSE:
+            if ex != bracket_couple[0] and ex != bracket_couple[1]:
+                continue
+            if left == bracket_couple[0] and ex == bracket_couple[1]:
+                if len(stack) == 0:
+                    return False
+                stack.pop()
+                skip = True
+                left = ''
+                if len(stack) > 0:
+                    left = stack[len(stack) - 1]
+        if not skip:
+            left = ex
+            stack.append(ex)
+
+    return len(stack) == 0
+if __name__ == '__main__':
+	print(balanced_parentheses('(()())({})[]'))#True
+    print(balanced_parentheses('((ali)(koyuncu))({})[]'))#True
+    print(balanced_parentheses('(()())())'))#False
